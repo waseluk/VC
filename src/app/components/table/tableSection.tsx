0a0ac17aedefd6b6d6839f962fea2bddb808b8d0
waseluk <103688464+waseluk@users.noreply.github.com>
2025-09-16 15:40:13 +0100

    header tweaks

--- src/app/components/table/tableSection.tsx
+++ src/app/components/table/tableSection.tsx
@@ -6,14 +6,14 @@ export default function TableSection() {
   return (
     <section className={styles.workFormat}>
       <div className={styles.workIntro}>
-        <h3>
+        <h3 className={styles.headerSection}>
           The average effort of projects I launched in 2024 was{" "}
           <span>100 hours</span>. My smallest engagement was 25 hours, and the
           highest was 200 hours.
         </h3>
       </div>
       <div className={styles.workIntro}>
-        <h4>
+        <h4 className={styles.headerSection}>
           Not sure how long your project will take?{" "}
           <a href="mailto:hello@vasselcreative.com?subject=Let’s%20Work%20Together&body=Hi%20Vassel%20Creative,">
             Let’s have a <span>consultation</span>.
